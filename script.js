f89c3f2925541a27f92783161fd58517d64f2e85
lemauto <lemauriciokta@hotmail.com>
2025-12-15 05:52:56 -0300

    modificação 2

--- script.js
+++ script.js
@@ -40,7 +40,7 @@ searchInput.addEventListener("keypress", e => {
 });
 
 /* ================= NOVO: ESPELHAMENTO BACKEND ================= */
-const API_URL = "https://cadastrocatagoloesfera.infinityfreeapp.com/API/listar.php";
+const API_URL = "https://cadastrocatagoloesfera.infinityfreeapp.com/API/upload.php";
 const galeria = document.getElementById("galeria");
 
 const modal = document.getElementById("modal");
